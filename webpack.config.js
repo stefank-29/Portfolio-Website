95d69ab8140b5ba90fd65f52794154d8d4ac2c09
stefan <stefankaraferovic@gmail.com>
2020-08-27 13:16:59 +0200

    Project structure

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// webpack.config.js
+//import webpack from "webpack";
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    publicPath: "dist",
+  },
+  module: {
+    // kako da se tretiraju fajlovi pre nego sto se ubace u dependency graph
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.jpe?g$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    // razne funkcionalnosti
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false,
+    //   },
+    //   output: {
+    //     comments: false,
+    //   },
+    //   sourceMap: true,
+    // }),
+    //new webpack.ContextReplacementPlugin(),
+  ],
+};
