914952057a82792003484b79e75e74336da40d7d
stefan <stefankaraferovic@gmail.com>
2020-08-28 13:20:11 +0200

    Particles background

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,43 @@
 //import webpack from "webpack";
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    publicPath: "dist",
-  },
-  module: {
-    // kako da se tretiraju fajlovi pre nego sto se ubace u dependency graph
-    rules: [
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
-      {
-        test: /\.jpe?g$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-    ],
-  },
-  plugins: [
-    // razne funkcionalnosti
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false,
-    //   },
-    //   output: {
-    //     comments: false,
-    //   },
-    //   sourceMap: true,
-    // }),
-    //new webpack.ContextReplacementPlugin(),
-  ],
+	mode: 'development',
+	entry: {app: './src/index.js', particles: './src/particles.js'},
+	output: {
+		filename: '[name].bundle.js',
+		publicPath: 'dist',
+	},
+	module: {
+		// kako da se tretiraju fajlovi pre nego sto se ubace u dependency graph
+		rules: [
+			{test: /\.css$/, use: ['style-loader', 'css-loader']},
+			{
+				test: /\.jpe?g$/,
+				use: ['file-loader'],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		// razne funkcionalnosti
+		// new webpack.optimize.UglifyJsPlugin({
+		//   compress: {
+		//     warnings: false,
+		//   },
+		//   output: {
+		//     comments: false,
+		//   },
+		//   sourceMap: true,
+		// }),
+		//new webpack.ContextReplacementPlugin(),
+	],
 };
