c03e3fcf657a278cdc0156923e2728ec29c32630
stefan <stefankaraferovic@gmail.com>
2020-09-05 19:07:58 +0200

    Menu social logos

--- src/index.js
+++ src/index.js
@@ -11,18 +11,10 @@ particlesJS.load('particles-js', './particles.json', function () {
 
 window.onload = shellAnimation.insertLetter();
 
-//?menu
-//todo menu sa desne strane(moze da se minimizira)
-
-//?pozadina
-//todo dark/lighth tema(toggle light class)
-//todo podeliti particles na about i contact(vise tackica)
-
 //?content
 //todo ucitavanje kontenta kako skrolujem(AOS biblioteka)
 
 //?footer
-//todo visi
 //todo linkovi za linkedin i facebook
 //todo strelica za top of page(^^)
 
