408c3f7dd38b20a0039e527c51adbc0966e033ac
stefan <stefankaraferovic@gmail.com>
2020-09-05 15:33:09 +0200

    Section headers toggle mode

--- src/toggleTheme.js
+++ src/toggleTheme.js
@@ -4,9 +4,17 @@ const toggleTheme = (() => {
 	const checkbox = document.querySelector('#checkbox');
 	const body = document.querySelector('body');
 	const particles = document.querySelector('#particles-js');
+	const sectionHeaders = document.querySelectorAll('.section-header');
+	const dotsP = document.querySelectorAll('.dots');
 	function toggle() {
 		body.classList.toggle('light');
 		particles.classList.toggle('light');
+		sectionHeaders.forEach((sectionHeader) => {
+			sectionHeader.classList.toggle('light');
+		});
+		dotsP.forEach((dot) => {
+			dot.classList.toggle('light');
+		});
 	}
 
 	checkbox.addEventListener('change', toggle);
