b02266d5807404a25ddc7f8e7f2b982d0e753906
stefan <stefankaraferovic@gmail.com>
2020-09-05 12:16:35 +0200

    Menu position

--- src/index.js
+++ src/index.js
@@ -8,3 +8,27 @@ particlesJS.load('particles-js', './particles.json', function () {
 });
 
 window.onload = shellAnimation.insertLetter();
+
+//?menu
+//todo menu sa desne strane(moze da se minimizira)
+
+//?pozadina
+//todo dark/lighth tema(toggle light class)
+//todo podeliti particles na about i contact(vise tackica)
+
+//?content
+//todo ucitavanje kontenta kako skrolujem(AOS biblioteka)
+
+//?footer
+//todo visi
+//todo linkovi za linkedin i facebook
+//todo strelica za top of page(^^)
+
+//*kasnije
+
+//?responsive
+//todo napraviti da bude responsive
+
+//?hosting
+//todo freedom
+//todo github pages + custom domain
