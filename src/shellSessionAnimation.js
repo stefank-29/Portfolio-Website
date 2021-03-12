88e1744d6d98e09d8e9df13f01689dad685026c6
stefank-29 <stefankaraferovic@gmail.com>
2021-03-12 16:05:09 +0100

    home page

--- src/shellSessionAnimation.js
+++ src/shellSessionAnimation.js
@@ -2,7 +2,7 @@ const shellAnimation = (() => {
 	const shell = document.querySelector('#shell-text');
 	const text = `I am skilled with web development languages and frameworks such as JavaScript, React.js, Vue.js, Node.js,
 	Express.js, Keystone.js, GraphQL, CSS3, HTML5 etc. Also, have experience working with object-oriented programming languages such as
-	Python, Java and C++, databases (MongoDB, MySQL), and industry tools such as (Webpack, Git, Github, ESLint).`;
+	Python, Java and C++, databases (MongoDB, MySQL), and industry tools such as (Webpack, Git, Github, ESLint). `;
 	const textArray = text.split('');
 	let timer;
 	function insertLetter() {
